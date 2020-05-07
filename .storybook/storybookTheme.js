23b52b343e388f48f4e9e8b62207465ab990932b
Johan De Geer <johan.de.geer@whitespace.se>
2020-05-07 10:58:39 +0400

    Added theme settings

---
+++ .storybook/storybookTheme.js
@@ -0,0 +1,38 @@
+import { create } from "@storybook/theming/create";
+import logo from "./public/logo.svg";
+
+export default create({
+  base: "light",
+
+  colorPrimary: "#ff5001",
+  colorSecondary: "#215da5",
+
+  // UI
+  appBg: "white",
+  appContentBg: "white",
+  appBorderColor: "white",
+  appBorderRadius: 0,
+
+  // Typography
+  fontBase: "Inter",
+  fontCode: "monospace",
+
+  // Text colors
+  textColor: "black",
+  textInverseColor: "rgba(255,255,255,0.9)",
+
+  // Toolbar default and active colors
+  barTextColor: "black",
+  barSelectedColor: "black",
+  barBg: "lightgrey",
+
+  // Form colors
+  inputBg: "white",
+  inputBorder: "grey",
+  inputTextColor: "black",
+  inputBorderRadius: 0,
+
+  brandTitle: "Whitespace",
+  brandUrl: "https://www.whitespace.se",
+  brandImage: `/${logo}`,
+});
