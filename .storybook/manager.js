458c33a9c2417a7a0e0dd3834c883f0d75d6e669
Johan De Geer <johan.de.geer@whitespace.se>
2020-05-08 16:28:05 +0400

    Added default config

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -3,4 +3,24 @@ import storybookTheme from "./storybookTheme";
 
 addons.setConfig({
   theme: storybookTheme,
+  isFullscreen: false, // show story component as full screen
+  showNav: true, // display panel that shows a list of stories
+  showPanel: false, // display panel that shows addon configurations
+  panelPosition: "bottom", // here to show the addon panel
+  sidebarAnimations: false, // sidebar tree animations
+  enableShortcuts: false, // enable/disable shortcuts
+  isToolshown: true, // show/hide tool bar
+  selectedPanel: undefined, // id to select an addon panel
 });
+
+// addons.setConfig({
+//   theme: undefined,
+//   isFullscreen: false, // show story component as full screen
+//   showNav: true, // display panel that shows a list of stories
+//   showPanel: true, // display panel that shows addon configurations
+//   panelPosition: "bottom", // here to show the addon panel
+//   sidebarAnimations: true, // sidebar tree animations
+//   enableShortcuts: true, // enable/disable shortcuts
+//   isToolshown: true, // show/hide tool bar
+//   selectedPanel: undefined, // id to select an addon panel
+// });
